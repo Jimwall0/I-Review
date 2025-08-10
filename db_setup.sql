a14e7c20d59e3f00e33f5c9d373c58cfb1d56c3a
Tsim Muaj <127359266+Jimwall0@users.noreply.github.com>
2025-08-10 14:26:31 -0500

    first table

---
+++ db_setup.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS media_catalog;
+CREATE USER IF NOT EXISTS 'Andy'@'localhost' IDENTIFIED BY '@ver@g3V#1w3r';
+GRANT ALL PRIVILEGES ON media_catalog.* TO 'Andy'@'localhost';
+FLUSH PRIVILEGES;
+USE media_catalog;
+
+CREATE TABLE IF NOT EXISTS media (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  watched BOOLEAN DEFAULT FALSE,
+  type ENUM('movie', 'show') NOT NULL,
+  type ENUM('Cracked', 'Wack') NOT NULL,
+  review text,
+);
